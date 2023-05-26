acd3147811f1aa375091f6b42f331f6794031634
Lihini Witharana <cb010846@students.apiit.lk>
2023-05-26 23:01:48 +0530

    Updated code with working Collections Page and a DateCalculatorHook

---
+++ src/Hooks/useDateCalculator.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+
+
+const useDateCalculator = () => {
+    const [borrowedDate, setBorrowedDate] = useState();
+    const [returnedDate, setReturnedDate] = useState();
+    const [DaysLeft, setDaysLeft] = useState(null);
+    const [DaysOverdue, setDaysOverdue] = useState(null);
+ 
+
+    
+    const processDays = (borroweddate, returneddate) => {
+        let payload = {};
+        payload = { ...payload};
+        const currentDate = new Date();
+        const dueDate = new Date(borroweddate)
+        dueDate.setDate(dueDate.getDate() + 14); // Assuming the due date is 14 days after borrowing
+        const timeDifference = dueDate.getTime() - currentDate.getTime();
+        const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+        payload = {
+            ...payload,
+            daysLeft: daysDifference > 0 ? daysDifference : 0,
+            daysOverdue: daysDifference < 0 ? Math.abs(daysDifference) : 0,
+            borrowedDate: new Date(borroweddate),
+            returnedDate: returneddate !== "" ? new Date(returneddate) : ""
+        }
+        return payload;
+    }
+
+    return {
+        DaysLeft,
+        DaysOverdue,
+        fines: "200",
+        processDays
+    };
+}
+
+
+
+export default useDateCalculator
--- src/screens/Collection.js
+++ src/screens/Collection.js
@@ -6,19 +6,18 @@ import { colors, parameters, TextStyle } from "../global/styles.js"
 import Logo from '../componants/Logo.js'
 import firebase from '@react-native-firebase/app';
 import database from '@react-native-firebase/database';
-import useDateCalculator from '../logic/useDateCalculator.js';
+import useDateCalculator from '../Hooks/useDateCalculator.js';
 
 
 export default function Collections() {
-  const CurrentMemberId = "m1";
+  const CurrentMemberId = "m3";
   const [books, setBooks] = useState([]);
   const { processDays } = useDateCalculator();
 
   useEffect(() => {
     const Reference = firebase.app().database('https://library-project-efd46-default-rtdb.asia-southeast1.firebasedatabase.app/');
     const LoansReference = Reference.ref('loans');
-    const sortedmemberLoanRef = LoansReference.orderByChild("memberid");
-    const memberLoanRef = sortedmemberLoanRef.equalTo(CurrentMemberId);
+    const memberLoanRef = LoansReference.orderByChild("memberid").equalTo(CurrentMemberId);
     const booksArray = [];
 
     memberLoanRef.on('value', (snapshot) => {
@@ -26,9 +25,15 @@ export default function Collections() {
       Object.keys(memberLoans).forEach((loanKey) => {
         const loan = memberLoans[loanKey];
         const bookID = loan.bookid;
-        const { borrowedDate, returnedDate } = loan;
-        const { returnStatus, daysLeft, daysOverdue, } = processDays(borrowedDate, returnedDate);
-
+        const { loandate, returneddate } = loan;
+    
+        const borrowedDate = loan.loandate
+        const returnedDate = loan.returneddate
+        console.log("borrowed and returned dates")
+        console.log( borrowedDate)
+        console.log(returnedDate)
+        
+        const { daysLeft, daysOverdue} = processDays(borrowedDate, returnedDate);
         const bookReference = Reference.ref('Books/' + bookID);
         bookReference.on('value', (snapshot) => {
           const book = snapshot.val();
@@ -41,20 +46,30 @@ export default function Collections() {
               author: bookAuthor,
               daysleft: daysLeft,
               daysoverdue: daysOverdue,
-              returnstatus: returnStatus
+              returnstatus: !(returneddate === "")
             });
 
             setBooks([...booksArray]);
           } else {
             console.log("the book isn't available man");
           }
+     
         });
       });
     });
   }, []);
 
   return (
+
+    
+
     <View style={styles.container}>
+         <View style={styles.header}>
+        <Text style={styles.headingpage}>My Collection</Text>
+        <Button style={styles.button} icon="bookmark-multiple" mode="contained" buttonColor={colors.Peach} textColor={colors.Black} onPress={() => console.log('Pressed')}>
+          Waitlist
+        </Button>
+      </View>
       {/* Render a BookCard component for each book in the array */}
       {books.map((book, index) => (
         <BookCard key={index} BookTitle={book.title} BookAuthor={book.author} daysLeft={book.daysleft} daysOverdue={book.daysoverdue} returnedStatus={book.returnstatus} />
