b0de1d577b3b58e6e9ebd116939120c65c849982
chanka-herath2001 <113530540+chanka-herath2001@users.noreply.github.com>
2023-06-15 10:59:26 +0530

    Add files via upload

---
+++ LibrarianLoans.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Dimensions, TextInput, Button,Image, TouchableOpacity} from 'react-native'
+import {colors, parameters, TextStyle} from "../global/styles.js"
+
+
+export default function MyAccount()
+{
+    return( 
+    <View style={styles.Test}>
+        <Text style={styles.Test}>Librarian loan page</Text>
+    </View>
+    );
+   
+}
+
+const styles = StyleSheet.create({
+    Test:{
+        color:'#fff',
+        backgroundColor:'#4d2959',
+        marginTop:20,
+        flex:1
+    }
+})
